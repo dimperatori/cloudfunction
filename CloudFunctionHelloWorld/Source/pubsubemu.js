a45ed703344f0937716d9160c54667abb42705e1
dimperatori <nicoin2@hotmail.com>
2020-07-07 11:27:29 -0700

    Add project files.

---
+++ CloudFunctionHelloWorld/Source/pubsubemu.js
@@ -0,0 +1,15 @@
+﻿const c = require('./function-1.js');
+
+const fs = require('fs');
+
+const requestBuffer = fs.readFileSync('./data.json', "utf8");
+const requestJSON = requestBuffer.toString().trim();
+
+//Parse file in object
+const event = JSON.parse(requestJSON);
+
+dataJSON = JSON.stringify(event.data);
+//Encode data in Base64
+event.data = Buffer.from(dataJSON).toString('base64');
+
+c.helloPubSub(event, 'this is the context');
